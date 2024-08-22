54cc262271f6f365737703a02bec5189627c04d1
DragonQuick <ilyosbek0215@gmail.com>
2024-08-22 22:08:43 +0500

    END CODE

--- js/script.js
+++ js/script.js
@@ -1,37 +1,87 @@
+// GLOBAL VARIABLE
 const canvas = document.querySelector("canvas"),
-  toolBtns = document.querySelectorAll(".tool");
+  toolBtns = document.querySelectorAll(".tool"),
+  fillColor = document.querySelector("#fill-color"),
+  sizeSlider = document.querySelector("#size-slider"),
+  colorBtns = document.querySelectorAll(".colors .option"),
+  colorPicker = document.querySelector(".color-picker"),
+  clearCanvasBtn = document.querySelector(".clear-canvas"),
+  saveImageBtn = document.querySelector(".save-img");
 
+// VARIABLE
 let ctx = canvas.getContext("2d"),
   isDrawing = false,
   brushWidth = 5,
   selectedTool = "brush",
+  selectedColor = "#000",
   prevMouseX,
   prevMouseY,
   snapshot;
 
+// FOR WHITE BACKGROUND
+const setCanvasBackground = () => {
+  ctx.fillStyle = "#fff";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = selectedColor;
+};
+
+// CANVA SIZE
 window.addEventListener("load", () => {
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
+  setCanvasBackground();
 });
 
+// START DRAWING
 const startDraw = (e) => {
   isDrawing = true;
   prevMouseX = e.offsetX;
   prevMouseY = e.offsetY;
   ctx.beginPath();
   ctx.lineWidth = brushWidth;
+  ctx.strokeStyle = selectedColor;
+  ctx.fillStyle = selectedColor;
   snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 };
 
+// DRAW RECTANGLE
 const drawRectangle = (e) => {
-  ctx.strokeRect(
-    e.offsetX,
-    e.offsetY,
-    prevMouseX - e.offsetX,
-    prevMouseY - e.offsetY
-  );
+  fillColor.checked
+    ? ctx.fillRect(
+        e.offsetX,
+        e.offsetY,
+        prevMouseX - e.offsetX,
+        prevMouseY - e.offsetY
+      )
+    : ctx.strokeRect(
+        e.offsetX,
+        e.offsetY,
+        prevMouseX - e.offsetX,
+        prevMouseY - e.offsetY
+      );
 };
 
+// DRAW CIRCLE
+const drawCircle = (e) => {
+  ctx.beginPath();
+  const radius = Math.sqrt(
+    Math.pow(prevMouseX - e.offsetX, 2) + Math.pow(prevMouseY - e.offsetY, 2)
+  );
+  ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI);
+  fillColor.checked ? ctx.fill() : ctx.stroke();
+};
+
+// DRAW TRIANGLE
+const drawTriangle = (e) => {
+  ctx.beginPath();
+  ctx.moveTo(prevMouseX, prevMouseY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.lineTo(prevMouseX * 2 - e.offsetX, e.offsetY);
+  ctx.closePath();
+  fillColor.checked ? ctx.fill() : ctx.stroke();
+};
+
+// DRAWING
 const drawing = (e) => {
   if (!isDrawing) return;
   ctx.putImageData(snapshot, 0, 0);
@@ -44,9 +94,21 @@ const drawing = (e) => {
     case "rectangle":
       drawRectangle(e);
       break;
+    case "circle":
+      drawCircle(e);
+      break;
+    case "triangle":
+      drawTriangle(e);
+      break;
+    case "eraser":
+      ctx.strokeStyle = "#fff";
+      ctx.lineTo(e.offsetX, e.offsetY);
+      ctx.stroke();
+      break;
   }
 };
 
+// SELECTED TOOL BUTTONS
 toolBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     document.querySelector(".options .active").classList.remove("active");
@@ -55,6 +117,44 @@ toolBtns.forEach((btn) => {
   });
 });
 
+// SIZE DRAWING
+sizeSlider.addEventListener("change", () => {
+  brushWidth = sizeSlider.value;
+});
+
+// SELECT COLOR BUTTONS
+colorBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    document.querySelector(".options .selected").classList.remove("selected");
+    btn.classList.add("selected");
+    const bgColor = window
+      .getComputedStyle(btn)
+      .getPropertyValue("background-color");
+    selectedColor = bgColor;
+  });
+});
+
+// COLORPICKER VALUE
+colorPicker.addEventListener("change", () => {
+  colorPicker.parentElement.style.background = colorPicker.value;
+  colorPicker.parentElement.click();
+});
+
+// CLEAR CANVAS
+clearCanvasBtn.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  setCanvasBackground();
+});
+
+// SAVE IMAGE
+saveImageBtn.addEventListener("click", () => {
+  const link = document.createElement("a");
+  link.download = `Dragon-paint${Date.now()}.jpg`;
+  link.href = canvas.toDataURL();
+  link.click();
+});
+
+// STOP DRAWING
 const stopDrawing = () => {
   isDrawing = false;
 };
